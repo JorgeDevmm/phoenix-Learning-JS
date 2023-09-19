ee42050bb6b84e9eb6066f795916cf78a69e3b06
JorgeDevmm <jorge29luis10@gmail.com>
2023-09-19 00:02:05 -0500

    Agregando JS0

---
+++ JS_0.js
@@ -0,0 +1,45 @@
+// JS I: Nombrando Variables en Js.
+
+//  Crea variables siguiendo las reglas de nomenclatura adecuadas.
+
+//1) Crea 10 variables de tipo string (cadena de texto) y asignale cualquier texto.
+// let variableString = "Hola, mundo!";
+
+let nombre1 = 'jorge';
+let fruta = 'manzana';
+let fruta2 = 'pera';
+let nombre2 = 'luis';
+let nombre3 = 'pedro';
+let nombre4 = 'maria';
+let nombre5 = 'luisa';
+let fruta3 = 'platano';
+let fruta4 = 'naranja';
+let nombre7 = 'juan';
+
+//2) Crea 10 variables de tipo number (numero) y asígnale cualquier número.
+// let variableNumber = 100;
+
+let numero1 = 1;
+let numero2 = 2;
+let numero3 = 3;
+let numero4 = 4;
+let numero5 = 5;
+let numero6 = 6;
+let numero7 = 7;
+let numero8 = 8;
+let numero9 = 9;
+let numero10 = 10;
+
+//3) Crea 10 variables de tipo boolean (true/false) y asígnale cualquier valor.
+// let variableBoolean = true;
+
+let valor1 = true;
+let valor2 = false;
+let valor3 = true;
+let valor4 = false;
+let valor5 = true;
+let valor6 = false;
+let valor7 = true;
+let valor8 = false;
+let valor9 = true;
+let valor10 = false;
